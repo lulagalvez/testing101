f3cab18bc4f2e0b54b8dc3f0a6fe6f62cf90cd47
lulagalvez <luisgalvezuk@gmail.com>
2024-04-03 22:15:30 -0300

    Agregado argumentos a funciones

--- ohce.py
+++ ohce.py
@@ -1,12 +1,15 @@
 
-def greet():
+from datetime import time
 
+def greet(name, t):
+    return
+
+def isPalindrome(s1,s2):
     return
 
 def reverseWord(s):
     return s[::-1]
 
-def stop():
-    
+def stop(s):
     return
 
