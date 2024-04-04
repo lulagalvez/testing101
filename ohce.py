a85dc6c161b594c44955d9166f3cc632c0dfdf1b
lulagalvez <luisgalvezuk@gmail.com>
2024-04-03 22:26:05 -0300

    Agregado logica para funcion de saludar que pasa los tests

--- ohce.py
+++ ohce.py
@@ -2,7 +2,13 @@
 from datetime import time
 
 def greet(name, t):
-    return
+    hour = t.hour
+    if 20 <= hour or hour < 6:
+        return f'¡Buenas noches {name}!'
+    elif 6 <= hour < 12:
+        return f'¡Buenos días {name}!'
+    else:
+        return f'¡Buenas tardes {name}!'
 
 def isPalindrome(s1,s2):
     return
--- ohce_test.py
+++ ohce_test.py
@@ -22,24 +22,24 @@ def test_isnt_palindrome():
 
 def test_greet_morning():
     name = "Lula"
-    assert ohce.greet(name, time(10,10)) == f"¡Buenos días {name}"
-    assert ohce.greet(name, time(6,30)) == f"¡Buenos días {name}"
-    assert ohce.greet(name, time(12,0)) == f"¡Buenos días {name}"
-    assert ohce.greet(name, time(6,0)) == f"¡Buenos días {name}"
+    assert ohce.greet(name, time(10,10)) == f"¡Buenos días {name}!"
+    assert ohce.greet(name, time(6,30)) == f"¡Buenos días {name}!"
+    assert ohce.greet(name, time(11,59)) == f"¡Buenos días {name}!"
+    assert ohce.greet(name, time(6,0)) == f"¡Buenos días {name}!"
 
 def test_greet_afternoon():
     name = "Lula"
-    assert ohce.greet(name, time(13,10)) == f"¡Buenas tardes {name}"
-    assert ohce.greet(name, time(15,30)) == f"¡Buenas tardes {name}"
-    assert ohce.greet(name, time(12,1)) == f"¡Buenas tardes {name}"
-    assert ohce.greet(name, time(19,59)) == f"¡Buenas tardes {name}"
+    assert ohce.greet(name, time(13,10)) == f"¡Buenas tardes {name}!"
+    assert ohce.greet(name, time(15,30)) == f"¡Buenas tardes {name}!"
+    assert ohce.greet(name, time(12,1)) == f"¡Buenas tardes {name}!"
+    assert ohce.greet(name, time(19,59)) == f"¡Buenas tardes {name}!"
 
 def test_greet_night():
     name = "Lula"
-    assert ohce.greet(name, time(20,0)) == f"¡Buenas noches {name}"
-    assert ohce.greet(name, time(21,57)) == f"¡Buenas noches {name}"
-    assert ohce.greet(name, time(4,10)) == f"¡Buenas noches {name}"
-    assert ohce.greet(name, time(5,0)) == f"¡Buenas noches {name}"
+    assert ohce.greet(name, time(20,0)) == f"¡Buenas noches {name}!"
+    assert ohce.greet(name, time(21,57)) == f"¡Buenas noches {name}!"
+    assert ohce.greet(name, time(4,10)) == f"¡Buenas noches {name}!"
+    assert ohce.greet(name, time(5,0)) == f"¡Buenas noches {name}!"
 
 def test_stop():
     name = "Lula"
